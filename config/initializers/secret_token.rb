a54d4ea82745f86e2dfd326b34a708f6499fcb77
Brian Flanagan <b@flngn.com>
2014-03-20 11:47:16 +0000

    initial rails app with travis and codeclimate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StadinJutut::Application.config.secret_key_base = '363562353dbea2ec6c8f2abd2d10880f2b7744d8f0dc7bd30347fbbb14fe8dd4b0d4971484145120abbdb0e51b4e13dc21a5aa1f80046c12a1353cf1e83f5231'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StadinJutut::Application.config.session_store :cookie_store, key: '_StadinJutut_session'
